8178d56498db7bc872fce9f284a048508f36ee14
Lslinux Official <contact@lslinux.org>
2021-11-10 11:10:33 +0100

    fix install.sh : modules/examples in correct dir

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ BINDIR="${PREFIX}/bin"
 for f in LICENSE README.md; do install -v -m 644 -oroot -groot "${f}" "${SHAREDIR}/${f}"; done
 
 [ -d "${SHAREDIR}/modules" ] || install -v -d -m755 -oroot -groot "${SHAREDIR}/modules"
-for m in modules/*; do install -v -m644 -oroot -groot "${m}" "${SHAREDIR}/modules/${m}"; done
+for m in modules/*; do install -v -m644 -oroot -groot "${m}" "${SHAREDIR}/${m}"; done
 
 [ -d "${SHAREDIR}/examples" ] || install -v -d -m755 -oroot -groot "${SHAREDIR}/examples"
-for e in examples/*; do install -v -m755 -oroot -groot "${e}" "${SHAREDIR}/examples/${e}"; done
+for e in examples/*; do install -v -m755 -oroot -groot "${e}" "${SHAREDIR}/${e}"; done
 
 install -v -m644 -oroot -groot ${PROGNAME}.conf.example ${CONFDIR}/${PROGNAME}.conf.example
 [ -e "${CONFDIR}/${PROGNAME}.conf" ] || install -v -m 644 -oroot -groot ${PROGNAME}.conf.example ${CONFDIR}/${PROGNAME}.conf
